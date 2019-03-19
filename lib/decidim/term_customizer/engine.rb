43bac03c749d849a91aa4c76da1e024e92b7b903
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2019-03-19 12:44:48 +0200

    Apply term customizer for the jobs
    This applies term customizer also for the jobs in case the context
    is passed in the job's arguments containing either the target
    organization or the target user object.
    
    Allows customizing e.g. the email notification terms.

--- lib/decidim/term_customizer/engine.rb
+++ lib/decidim/term_customizer/engine.rb
@@ -44,6 +44,43 @@ module Decidim
           # Force the backend to reload the translations for the current request
           customizer_backend.reload!
         end
+
+        # The jobs are generally run in different context than the controllers
+        # which causes the term customizations not to be active. During the
+        # jobs, only the organization and global context translations are loaded
+        # because otherwise this would have to be job specific.
+        #
+        # Currently this has been only tested against the event notification
+        # jobs but it may work for other jobs as well. Because the jobs
+        # themselves don't carry any other context information than their
+        # arguments, it is difficult to resolve their correct context. Note also
+        # that e.g. the email notifications are always created through a single
+        # job that may be fired by another job (i.e. the notification job is
+        # always performed last).
+        ActiveSupport::Notifications.subscribe "perform_start.active_job" do |_name, _started, _finished, _unique_id, data|
+          # Figure out the organization and user through the job arguments if
+          # passed for the job.
+          organization = nil
+          user = nil
+          data[:job].arguments.each do |arg|
+            organization = arg if arg.is_a?(Decidim::Organization)
+            user = arg if arg.is_a?(Decidim::User)
+          end
+
+          # In case an organization was not passed for the job, check it through
+          # the user.
+          organization = user.organization if organization.nil? && user
+
+          # Create resolver for the target organization or global context in
+          # case organization was not found
+          resolver = Resolver.new(organization, nil, nil)
+
+          # Create the loader for the backend to fetch the translations from
+          TermCustomizer.loader = Loader.new(resolver)
+
+          # Force the backend to reload the translations for the job
+          customizer_backend.reload!
+        end
       end
     end
   end
--- spec/lib/decidim/term_customizer/engine_spec.rb
+++ spec/lib/decidim/term_customizer/engine_spec.rb
@@ -7,6 +7,9 @@ describe Decidim::TermCustomizer::Engine do
     ActiveSupport::Notifications.unsubscribe(
       "start_processing.action_controller"
     )
+    ActiveSupport::Notifications.unsubscribe(
+      "perform_start.active_job"
+    )
   end
 
   it "sets up term customizer i18n backend during initialization" do
@@ -82,6 +85,89 @@ describe Decidim::TermCustomizer::Engine do
 
         ActiveSupport::Notifications.instrument(
           "start_processing.action_controller",
+          dummy_data
+        )
+      end
+    end
+  end
+
+  context "when instrumenting the perform_start.active_job notification" do
+    let(:dummy_job) { double }
+    let(:dummy_data) { { job: dummy_job } }
+    let(:dummy_backend) { double }
+    let(:arguments) { [] }
+    let(:resolver) { double }
+
+    before do
+      allow(dummy_job).to receive(:job_id).and_return(1)
+      allow(dummy_job).to receive(:queue_name).and_return("queue")
+      allow(dummy_job).to receive(:arguments).and_return(arguments)
+    end
+
+    before do
+      expect(Decidim::TermCustomizer::I18nBackend).to receive(:new).and_return(dummy_backend)
+
+      run_initializer
+
+      expect(dummy_job).to receive(:arguments).and_return(arguments)
+    end
+
+    context "with organization in the arguments" do
+      let(:organization) { create(:organization) }
+      let(:arguments) { [organization] }
+
+      it "creates a resolver with the organization" do
+        expect(Decidim::TermCustomizer::Resolver).to receive(:new).with(
+          organization,
+          nil,
+          nil
+        ).and_return(resolver)
+        expect(Decidim::TermCustomizer::Loader).to receive(:new).with(resolver)
+        expect(dummy_backend).to receive(:reload!)
+
+        ActiveSupport::Notifications.instrument(
+          "perform_start.active_job",
+          dummy_data
+        )
+      end
+    end
+
+    context "with user in the arguments" do
+      let(:user) { create(:user) }
+      let(:arguments) { [user] }
+
+      it "creates a resolver with the user's organization" do
+        expect(Decidim::TermCustomizer::Resolver).to receive(:new).with(
+          user.organization,
+          nil,
+          nil
+        ).and_return(resolver)
+        expect(Decidim::TermCustomizer::Loader).to receive(:new).with(resolver)
+        expect(dummy_backend).to receive(:reload!)
+
+        ActiveSupport::Notifications.instrument(
+          "perform_start.active_job",
+          dummy_data
+        )
+      end
+    end
+
+    context "with user and organization in the arguments" do
+      let(:organization) { create(:organization) }
+      let(:user) { create(:user) }
+      let(:arguments) { [organization, user] }
+
+      it "creates a resolver with the organization" do
+        expect(Decidim::TermCustomizer::Resolver).to receive(:new).with(
+          organization,
+          nil,
+          nil
+        ).and_return(resolver)
+        expect(Decidim::TermCustomizer::Loader).to receive(:new).with(resolver)
+        expect(dummy_backend).to receive(:reload!)
+
+        ActiveSupport::Notifications.instrument(
+          "perform_start.active_job",
           dummy_data
         )
       end
