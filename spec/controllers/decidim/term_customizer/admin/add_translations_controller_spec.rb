5300e425704c1d1b6d1116258492483a47dc86e3
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2019-04-24 18:29:49 +0300

    Fix specs

--- spec/controllers/decidim/term_customizer/admin/add_translations_controller_spec.rb
+++ spec/controllers/decidim/term_customizer/admin/add_translations_controller_spec.rb
@@ -66,7 +66,7 @@ module Decidim
             expect(response).to have_http_status(:ok)
 
             json = JSON.parse(response.body)
-            expect(json.length).to eq(27)
+            expect(json.length).to eq(39)
           end
         end
       end
