2512bfbc50fd214923bedf54213f6bd8ba38cb31
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2019-04-30 09:02:50 +0300

    Add translations_admin_bulk.js to the JS asset manifest

--- app/assets/config/translation_sets_admin_manifest.js
+++ app/assets/config/translation_sets_admin_manifest.js
@@ -1,2 +1,3 @@
 //= link decidim/term_customizer/admin/translation_sets_admin.js
 //= link decidim/term_customizer/admin/translations_admin.js
+//= link decidim/term_customizer/admin/translations_admin_bulk.js
