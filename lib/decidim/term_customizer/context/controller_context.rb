5d7dca16af4f4cb049f1f573f6c5eefd8b427fc0
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2019-11-28 00:25:48 +0200

    Fix #28

--- lib/decidim/term_customizer/context/controller_context.rb
+++ lib/decidim/term_customizer/context/controller_context.rb
@@ -13,13 +13,15 @@ module Decidim
           # E.g. at the participatory process controller the
           # `decidim.current_participatory_space` environment variable has not
           # been set. Therefore, we need to fetch it directly from the
-          # controller using its private method.
-          @space =
-            if controller.respond_to?(:current_participatory_space, true)
-              controller.send(:current_participatory_space)
-            else
-              env["decidim.current_participatory_space"]
-            end
+          # controller using its private method. In some edge cases this may not
+          # be implemented (https://github.com/mainio/decidim-module-term_customizer/issues/28)
+          # in which case we do not have access to the participatory space.
+          if controller.respond_to?(:current_participatory_space, true)
+            @space = controller.try(
+              :current_participatory_space
+            )
+          end
+          @space ||= env["decidim.current_participatory_space"]
 
           @component = env["decidim.current_component"]
         end
