6bb12628310dc009355c7795b9ec0ba3dbcc2f82
Víctor <victor.ol@coditramuntana.com>
2020-12-11 10:38:43 +0100

    Add try to current_participatory_space call

--- lib/decidim/term_customizer/context/controller_context.rb
+++ lib/decidim/term_customizer/context/controller_context.rb
@@ -17,8 +17,8 @@ module Decidim
           # be implemented (https://github.com/mainio/decidim-module-term_customizer/issues/28)
           # in which case we do not have access to the participatory space.
           unless controller.respond_to?(:current_participatory_space)
-            controller.define_singleton_method(:public_current_participatory_space) { current_participatory_space }
-            @space = controller.try(:public_current_participatory_space)
+            controller.define_singleton_method(:public_current_participatory_space) { try(:current_participatory_space) }
+            @space = controller.public_current_participatory_space
           else
             @space = controller.try(:current_participatory_space)
           end
