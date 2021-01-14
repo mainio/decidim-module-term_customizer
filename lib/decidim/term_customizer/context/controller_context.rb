4c77c5d74707a0d1f6f04a2df252c88519bd3b97
Víctor <victor.ol@coditramuntana.com>
2021-01-14 12:47:40 +0100

    Fix not retrieving participatory processes on main show view page

--- lib/decidim/term_customizer/context/controller_context.rb
+++ lib/decidim/term_customizer/context/controller_context.rb
@@ -17,7 +17,9 @@ module Decidim
           # be implemented (https://github.com/mainio/decidim-module-term_customizer/issues/28)
           # in which case we do not have access to the participatory space.
           unless controller.respond_to?(:current_participatory_space)
-            controller.define_singleton_method(:public_current_participatory_space) { try(:current_participatory_space) }
+            controller.define_singleton_method(:public_current_participatory_space) do
+              current_participatory_space if self.respond_to? :current_participatory_space, true
+            end
             @space = controller.public_current_participatory_space
           else
             @space = controller.try(:current_participatory_space)
