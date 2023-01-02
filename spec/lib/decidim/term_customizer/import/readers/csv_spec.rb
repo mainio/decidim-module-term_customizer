5a8d4c67972ef10cd52e774d64a3c7cff9bc9325
Masayoshi Takahashi <takahashim@users.noreply.github.com>
2023-01-02 17:14:48 +0900

    Make col_sep in CSV importer configurable with `Decidim::TermCustomizer.csv_col_sep` (#89)
    * Make col_sep in CSV importer configurable with `Decidim::TermCustomizer.csv_col_sep`
    
    * Use Decidim.default_csv_col_sep
    
    Co-authored-by: Antti Hukkanen <antti.hukkanen@mainiotech.fi>
    
    * Remove `Decidim::TermCustomizer.csv_col_sep`
    
    Use `Decidim.default_csv_col_sep`
    
    Co-authored-by: Antti Hukkanen <antti.hukkanen@mainiotech.fi>

--- spec/lib/decidim/term_customizer/import/readers/csv_spec.rb
+++ spec/lib/decidim/term_customizer/import/readers/csv_spec.rb
@@ -3,7 +3,22 @@
 require "spec_helper"
 
 describe Decidim::TermCustomizer::Import::Readers::CSV do
-  let(:file) { file_fixture("set-translations.csv") }
+  context "when Decidim.default_csv_col_sep is `;`" do
+    let(:file) { file_fixture("set-translations.csv") }
 
-  it_behaves_like "translation import reader"
+    it_behaves_like "translation import reader"
+  end
+
+  context "when Decidim.default_csv_col_sep is `,`" do
+    let(:file) { file_fixture("set-translations-comma.csv") }
+
+    around do |example|
+      original_csv_col_sep = Decidim.default_csv_col_sep
+      Decidim.default_csv_col_sep = ","
+      example.run
+      Decidim.default_csv_col_sep = original_csv_col_sep
+    end
+
+    it_behaves_like "translation import reader"
+  end
 end
