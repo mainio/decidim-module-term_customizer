854ab3007b6b4d0556760f48af266a23123dc8eb
Joonas <joonas.aapro@mainiotech.fi>
2024-04-18 15:44:01 +0300

    Revert some changes

--- app/controllers/decidim/term_customizer/admin/application_controller.rb
+++ app/controllers/decidim/term_customizer/admin/application_controller.rb
@@ -5,7 +5,6 @@ module Decidim
     module Admin
       # This controller is the abstract class from which all other controllers
       # of this engine inherit.
-
       class ApplicationController < Decidim::Admin::ApplicationController
         def permission_class_chain
           [Decidim::TermCustomizer::Admin::Permissions] + super
